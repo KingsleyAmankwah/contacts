f0ee50d5a3d73e06842ca69c7c158a48f830d378
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-12-28 17:23:05 +0000

    users schema

---
+++ backend/models/user.ts
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose";
+// import { getRandomColorCode } from "../utils";
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please add first name"],
+    },
+    lastName: {
+      type: String,
+      default: null,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add password"],
+    },
+    // colorCode: {
+    //   type: String,
+    //   default: getRandomColorCode,
+    // },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    virtuals: {
+      name: {
+        get() {
+          return `${this.firstName} ${this.lastName || ""}`.trim();
+        },
+      },
+    },
+  }
+);
+
+const User = model("User", UserSchema);
+
+export default User;
