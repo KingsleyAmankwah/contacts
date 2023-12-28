2609c44e6966a07e9fc42ca57b6c70fc7636a3af
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-12-28 17:23:22 +0000

    router entry file

---
+++ backend/routes/index.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import UserRoutes from "./user";
+// import ContactRoutes from "./contact";
+// import LabelRoutes from "./label";
+
+const router = Router();
+
+router.use("/api/user", UserRoutes);
+// router.use("/api/contact", ContactRoutes);
+// router.use("/api/label", LabelRoutes);
+
+router.get("/api/health-check", (req, res) => {
+  res.status(200).send({
+    status: "Success",
+    data: {
+      message: "Service is running smoothly",
+      version: "1.0.0",
+    },
+  });
+});
+
+export default router;
