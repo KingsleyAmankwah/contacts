91f74d7dfd3da31d8d8dcc0e6dc175b94f1e00c9
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-12-28 17:21:02 +0000

    user controller

---
+++ backend/controllers/user.ts
@@ -0,0 +1,78 @@
+import asyncHandler from "express-async-handler";
+import {
+  ComparePassword,
+  CustomError,
+  HashPassword,
+  generateJwtToken,
+} from "../utils";
+import User from "../models/user";
+
+const signUp = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  if (!firstName || !lastName || !email || !password) {
+    throw CustomError(400, "Please fill all the fields");
+  }
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    throw CustomError(400, "User already exist");
+  }
+
+  const hashedPassword = await HashPassword(password);
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = generateJwtToken({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+  });
+
+  res.status(200).send({
+    data: { token },
+    success: true,
+    message: "Registration successful...",
+  });
+});
+
+const signIn = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw CustomError(400, "Please fill all the fields");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return CustomError(400, "Invalid credentials.");
+  }
+
+  const passwordIsCorrect = await ComparePassword(password, user.password);
+  if (!passwordIsCorrect) {
+    return CustomError(400, "Invalid Credentials");
+  }
+
+  const token = generateJwtToken({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+  });
+
+  res.status(200).send({
+    data: { token },
+    success: true,
+    message: `Welcome back, ${user.name}!`,
+  });
+});
+
+const UserController = { signIn, signUp };
+
+export default UserController;
