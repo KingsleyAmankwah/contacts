68d8eed39f0301b242d1fd859eef3f7cbbf21f8b
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-12-28 17:20:46 +0000

    server entry file

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./database/config";
+import router from "./routes";
+import ErrorHandler from "./middleware/errorHandler";
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
 
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(router);
+
+app.use(ErrorHandler);
 app.get("/", (req, res) => {
   res.send("Hello World! It's working🚀🔥🚀🔥🚀🔥🚀🔥🚀🔥");
 });
 
 app.listen(port, () => {
+  connectDB();
   console.log(`Server running at http://localhost:${port}`);
 });
