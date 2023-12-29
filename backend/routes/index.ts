08fc9686b25eb36ad9226ee5ffb6ac25541b44f8
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-12-29 08:50:38 +0000

    contact route and label routes added

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import UserRoutes from "./user";
-// import ContactRoutes from "./contact";
-// import LabelRoutes from "./label";
+import ContactRoutes from "./contact";
+import LabelRoutes from "./label";
 
 const router = Router();
 
 router.use("/api/user", UserRoutes);
-// router.use("/api/contact", ContactRoutes);
-// router.use("/api/label", LabelRoutes);
+router.use("/api/contact", ContactRoutes);
+router.use("/api/label", LabelRoutes);
 
 router.get("/api/health-check", (req, res) => {
   res.status(200).send({
