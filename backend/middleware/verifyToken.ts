71ad991ae60c55a0390c918309c18c41d753db57
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-12-29 08:51:59 +0000

    verify token modified

--- backend/middleware/verifyToken.ts
+++ backend/middleware/verifyToken.ts
@@ -1,13 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import asyncHandler from "express-async-handler";
 import { verify } from "jsonwebtoken";
-// import { asyncHandler } from "../utils";
 
 type User = {
   _id: string;
   name: string;
   email: string;
-  colorCode: string;
+  // colorCode: string;
 };
 
 declare global {
@@ -18,19 +17,24 @@ declare global {
   }
 }
 
-const verifyToken = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+const verifyToken = asyncHandler(async (req, res, next) => {
+  try {
     let token = req.headers.authorization;
 
-    if (!token) {
-      throw new Error("Unauthorized");
+    if (token && token.startsWith("Bearer ")) {
+      token = token.split(" ")[1]; // Extract the token part
     } else {
-      let decoded = await verify(token, process.env.JWT_SECRET as string);
-      req.user = decoded as User;
-      next();
+      throw new Error("Unauthorized: No token provided");
     }
+
+    const decoded = verify(token, process.env.JWT_SECRET as string);
+    req.user = decoded as User;
     next();
+  } catch (error) {
+    console.error("Token verification error:", error);
+    res.status(401).json({ message: "Unauthorized: Invalid token" });
+    throw error;
   }
-);
+});
 
 export default verifyToken;
