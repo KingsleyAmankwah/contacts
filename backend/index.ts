a3f8fa0760422ea40f0a8b9d9836337602383c4e
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-12-30 15:33:04 +0000

    ....

--- backend/index.ts
+++ backend/index.ts
@@ -6,17 +6,14 @@ import ErrorHandler from "./middleware/errorHandler";
 
 dotenv.config();
 const port = process.env.PORT;
-
 const app = express();
 
 app.use(express.json()).use(express.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
   res.send("Hello World! It's working🚀🔥🚀🔥🚀🔥🚀🔥🚀🔥");
 });
 
 app.use(router);
-
 app.use(ErrorHandler);
 
 app.listen(port, () => {
