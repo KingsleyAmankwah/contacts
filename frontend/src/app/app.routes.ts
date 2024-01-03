e176bed9492b4d88655ea132ca51d809486d4085
KingsleyAmankwah <kuamiklassic149@gmail.com>
2024-01-03 07:50:04 +0000

    app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const AppRoutingModule: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/auth/auth-routing.module').then((m) => m.AuthRoutes),
+  },
+
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/contact/contact-routing.module').then(
+        (m) => m.ContactRoutes
+      ),
+  },
+];
