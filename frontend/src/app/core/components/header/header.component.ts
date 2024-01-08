bc5cccbcac462dc4ce1d4d53901df561f0c6da5b
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-08 12:40:56 -0800

    header component

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -1,35 +1,34 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
-import { AuthService } from '../../../modules/auth/service/auth.service';
+import { AuthService } from '../../../auth/service/auth.service';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, MatMenuModule],
+  imports: [RouterLink, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   authService = inject(AuthService);
 
-  // get initial() {
-  //   if (!this.authService.user) return '';
-  //   let [firstLetter, secondLetter = ''] =
-  //     this.authService.user.name.split(' ');
-  //   return `${firstLetter[0].toUpperCase()}${secondLetter[0].toUpperCase()}`.trim();
-  // }
+  @Output() toggleSidebar = new EventEmitter<void>();
+
+  onToggleSidebar() {
+    this.toggleSidebar.emit(); // Call this method when the menu icon is clicked
+  }
 
   get initial() {
     if (!this.authService.user) return '';
     let [firstLetter, secondLetter = ''] =
       this.authService.user.name.split(' ');
+    return `${firstLetter[0].toUpperCase()}${secondLetter[0].toUpperCase()}`.trim();
+  }
 
-    firstLetter =
-      firstLetter && firstLetter[0] ? firstLetter[0].toUpperCase() : '';
-    secondLetter =
-      secondLetter && secondLetter[0] ? secondLetter[0].toUpperCase() : '';
-
-    return `${firstLetter}${secondLetter}`.trim();
+  get colorCode() {
+    return this.authService.user?.colorCode;
   }
 
   handleLogOut() {
