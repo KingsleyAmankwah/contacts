68328b1e7ba421cc3fafae9f1afad39548df0bd7
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-08 12:41:07 -0800

    app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,17 +5,14 @@ import { ContactGuard } from './core/guards/contact.guard';
 export const AppRoutingModule: Routes = [
   {
     path: 'auth',
-    // canActivate: [AuthGuard],
     loadChildren: () =>
-      import('./modules/auth/auth-routing.module').then((m) => m.AuthRoutes),
+      import('./auth/auth-routing.module').then((m) => m.AuthRoutes),
   },
 
   {
     path: 'contact',
     canActivate: [ContactGuard],
     loadChildren: () =>
-      import('./modules/contact/contact-routing.module').then(
-        (m) => m.ContactRoutes
-      ),
+      import('./contact/contact-routing.module').then((m) => m.ContactRoutes),
   },
 ];
