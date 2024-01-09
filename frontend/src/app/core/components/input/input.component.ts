9d3b5e1a5e132e0817ee06f6598986fba450eea6
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-08 16:56:45 -0800

    input component

---
+++ frontend/src/app/core/components/input/input.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css',
+})
+export class InputComponent {
+  @Input() hasError = false;
+  @Input() readOnly = false;
+  @Input() type = 'text';
+  @Input() name = '';
+  @Input() label = '';
+  @Input() control = new FormControl();
+  @Input() errorMessage: Record<string, string> = {};
+
+  get errorType() {
+    if (!this.control.errors) return;
+    return Object.keys(this.control.errors)[0];
+  }
+}
