3f65373be9254bce9d67aa4ad618cc337ea03f9d
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-09 11:22:22 -0800

    add contact

--- frontend/src/app/contact/pages/add-contact/add-contact.component.ts
+++ frontend/src/app/contact/pages/add-contact/add-contact.component.ts
@@ -1,16 +1,27 @@
-import { Component, EventEmitter, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  Output,
+  inject,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { ContactDetail } from '../../interface';
+import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-contact',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, ContactFormComponent],
   templateUrl: './add-contact.component.html',
   styleUrl: './add-contact.component.css',
 })
 export class AddContactComponent {
-  showImage!: boolean;
-  contactDetail = {} as ContactDetail;
-  onClose = new EventEmitter();
+  router = inject(Router);
+
+  handleCloseForm() {
+    this.router.navigateByUrl('/contact/list');
+  }
 }
