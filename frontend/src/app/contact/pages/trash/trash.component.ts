f34808c5ced06fc6a38c6548517ec32e5c3563de
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-08 16:58:50 -0800

    trash component

--- frontend/src/app/contact/pages/trash/trash.component.ts
+++ frontend/src/app/contact/pages/trash/trash.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ContactHeaderComponent } from '../../components/contact-header/contact-header.component';
 
 @Component({
   selector: 'app-trash',
   standalone: true,
-  imports: [],
+  imports: [ContactHeaderComponent],
   templateUrl: './trash.component.html',
-  styleUrl: './trash.component.css'
+  styleUrl: './trash.component.css',
 })
-export class TrashComponent {
-
-}
+export class TrashComponent {}
