80871faab6950725a61756cfb453949948dc00b6
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-08 16:59:02 -0800

    sidebar component

--- frontend/src/app/core/components/sidebar/sidebar.component.ts
+++ frontend/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BulkUploadPopupComponent } from '../../../contact/components/bulk-upload-popup/bulk-upload-popup.component';
+import { LabelPopupComponent } from '../../../contact/components/label-popup/label-popup.component';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,4 +14,20 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class SidebarComponent {
   count: number = 0;
+
+  dialog = inject(MatDialog);
+  @Output() onAddLabel = new EventEmitter();
+
+  showBulkUploadPopup() {
+    this.dialog.open(BulkUploadPopupComponent, {
+      width: '500px',
+    });
+  }
+
+  showLabelPopup() {
+    this.dialog.open(LabelPopupComponent, {
+      width: '500px',
+      // other configuration
+    });
+  }
 }
