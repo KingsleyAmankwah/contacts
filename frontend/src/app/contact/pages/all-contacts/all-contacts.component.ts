d23ff91282ef0b3d703fe86cb73162f187fc72c0
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-08 16:58:41 -0800

    all contacts component

--- frontend/src/app/contact/pages/all-contacts/all-contacts.component.ts
+++ frontend/src/app/contact/pages/all-contacts/all-contacts.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Contact } from '../../interface';
 import { ContactService } from '../../service/contact.service';
+import { ContactHeaderComponent } from '../../components/contact-header/contact-header.component';
+import { ContactListComponent } from '../../components/contact-list/contact-list.component';
 
 @Component({
   selector: 'app-all-contacts',
   standalone: true,
-  imports: [],
+  imports: [ContactHeaderComponent, ContactListComponent],
   templateUrl: './all-contacts.component.html',
   styleUrl: './all-contacts.component.css',
 })
