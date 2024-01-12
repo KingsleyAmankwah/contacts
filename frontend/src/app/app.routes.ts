d0be346027baebf73cae3ed92979ac93bc790cbb
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-12 12:59:25 -0800

    aws-3

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 import { ContactGuard } from './core/guards/contact.guard';
 import { LayoutComponent } from './auth/layout/layout.component';
+import { HomeComponent } from './home/home.component';
 
 export const AppRoutingModule: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'auth',
     loadChildren: () =>
@@ -22,4 +27,6 @@ export const AppRoutingModule: Routes = [
     redirectTo: '/auth/sign-in',
     pathMatch: 'full',
   },
+
+  { path: '**', redirectTo: '/auth/sign-in', pathMatch: 'full' },
 ];
