daa867cf0b9db375e22b6386ceea20ecd705328a
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-12 15:48:28 -0800

    edit contact page added to route

--- frontend/src/app/contact/contact-routing.module.ts
+++ frontend/src/app/contact/contact-routing.module.ts
@@ -3,6 +3,7 @@ import { LayoutComponent } from './layout/layout.component';
 import { AllContactsComponent } from './pages/all-contacts/all-contacts.component';
 import { AddContactComponent } from './pages/add-contact/add-contact.component';
 import { TrashComponent } from './pages/trash/trash.component';
+import { EditContactComponent } from './pages/edit-contact/edit-contact.component';
 
 export const ContactRoutes: Routes = [
   {
@@ -18,6 +19,12 @@ export const ContactRoutes: Routes = [
         path: 'add',
         component: AddContactComponent,
         title: 'Add Contact',
+      },
+
+      {
+        path: ':contactId/edit',
+        component: EditContactComponent,
+        title: 'Edit Contact',
       },
 
       { path: 'trash', component: TrashComponent, title: 'Trash' },
