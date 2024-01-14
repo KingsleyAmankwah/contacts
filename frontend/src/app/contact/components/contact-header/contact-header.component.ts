1e0e0b2fc77b7a70bdd91bd96567326ecdc53de1
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-13 16:22:52 -0800

    contact header

--- frontend/src/app/contact/components/contact-header/contact-header.component.ts
+++ frontend/src/app/contact/components/contact-header/contact-header.component.ts
@@ -1,11 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, NgIf],
   templateUrl: './contact-header.component.html',
   styleUrl: './contact-header.component.css',
 })
-export class ContactHeaderComponent {}
+export class ContactHeaderComponent {
+  isTrash = true;
+}
