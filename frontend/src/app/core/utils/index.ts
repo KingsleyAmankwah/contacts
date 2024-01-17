552f7e349d408cc8bde6e1e50bf2768db4b30d07
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-17 11:31:28 -0800

    Password validation pattern added

--- frontend/src/app/core/utils/index.ts
+++ frontend/src/app/core/utils/index.ts
@@ -33,3 +33,27 @@ export const cookie = {
     document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
   },
 };
+
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export class CustomValidators {
+  static passwordStrength() {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+        return null;
+      }
+      const hasUpperCase = /[A-Z]+/.test(value);
+      const hasLowerCase = /[a-z]+/.test(value);
+      const hasNumeric = /[0-9]+/.test(value);
+      const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(value);
+      const passwordValid =
+        hasUpperCase &&
+        hasLowerCase &&
+        hasNumeric &&
+        hasSpecial &&
+        value.length >= 8;
+      return !passwordValid ? { passwordStrength: true } : null;
+    };
+  }
+}
